47240672e7e55f90b666c664f88cdd2ff3a07897
Stephen Fluin <stephen@mistuph.com>
2009-12-09 23:48:22 -0600

    Continued implementation of scorecard, and AI naming

---
+++ hexconfig.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#
+# HexSLayer
+# copyright (C) Stephen Fluin 2009
+#
+
+tilesize = 32
+playerColors = ("#66FF33","#003DF5","#FF3366","#33FFCC","#FFCC33","#FF6633")
+playerNames = ("Human Player", "AI 1", "AI 2", "AI 3", "AI 4", "AI 5")
+
+infobarLocation =(25,455)
+storeLocation = (325,450)
+scoreLocation = (450,30)
