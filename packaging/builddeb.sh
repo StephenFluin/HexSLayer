ac56c4a9257caa2eb6a6dd3a48316624115e1650
Stephen Fluin <stephen@mistuph.com>
2010-01-09 17:38:50 -0600

    Updated build script to add images.

--- packaging/builddeb.sh
+++ packaging/builddeb.sh
@@ -2,7 +2,7 @@ mkdir -p debian/usr/share/pixmaps
 mkdir -p debian/usr/share/games/hexslayer
 
 cp ../gameicon.png debian/usr/share/pixmaps/hexslayer.png
-
+cp ../*.png debian/usr/share/games/hexslayer/
 py_compilefiles ../*.py
 mv ../*.pyc debian/usr/share/games/hexslayer
 
