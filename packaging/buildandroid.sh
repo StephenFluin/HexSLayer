3babbe269aa967b12c1ba8588ba83620bce50274
Stephen Fluin <stephen.fluin@gmail.com>
2012-02-06 20:03:03 -0600

    Updated version strings

--- packaging/buildandroid.sh
+++ packaging/buildandroid.sh
@@ -12,7 +12,7 @@ cp ../../version.txt tmp/
 
 cd pygame-package-0.9.3
 rm bin/*
-./build.py --package "com.mortalpowers.android.hexslayer" --name "HexSLayer" --version "`cat ../../../version.txt`" --numeric-version 9 --private ../tmp --permission INTERNET --icon /super/workspace/HexSLayer/gameicon.png debug
+./build.py --package "com.mortalpowers.android.hexslayer" --name "HexSLayer" --version "`cat ../../../version.txt`" --numeric-version 10 --private ../tmp --permission INTERNET --icon /super/workspace/HexSLayer/gameicon.png debug
 rm build.xml
 rm -r ../tmp
 android update project -p ./ -n HexSLayer
